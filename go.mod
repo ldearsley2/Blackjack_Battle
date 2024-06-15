2f5a5ebd308c5ee1ae8f2604b06af80fe7fb2414
ldearsley2 <ldearsley2@gmail.com>
2024-06-15 18:49:39 +0100

    Add player struct

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/ldearsley2/Blackjack_Battle
 
 go 1.22.3
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+)
--- main.go
+++ main.go
@@ -2,8 +2,34 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
 )
+
+type player struct {
+	id string
+	name string
+	points int
+	cards []string
+	isPlaying bool
+	conn *websocket.Conn
+}
+
+func newPlayer(name string, points int, conn *websocket.Conn) *player {
+	client := player{
+		id: uuid.New().String(),
+		name: name,
+		points: points,
+		cards: make([]string, 0),
+		isPlaying: false,
+		conn: conn,
+	}
+	return &client
+}
 
 func main() {
 	fmt.Println("Foobar")
+
+	
 }
